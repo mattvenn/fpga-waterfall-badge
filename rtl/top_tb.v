9d95c0585d37e47d0add5153dab69520c3881c6f
matt venn <matt@mattvenn.net>
2019-10-17 17:00:29 +0200

    sample period for debug

--- rtl/top_tb.v
+++ rtl/top_tb.v
@@ -37,11 +37,12 @@ module test;
         .LIMIT_BINS(32), 
         .H_VISIBLE(64),
         .V_VISIBLE(10),
+        .TWID_W(10),
         .GRADIENT_FILE("GRADIENT_GREY_24.hex")
         ) top_0( .clock_in(clk), .adc_mic_clk(adc_clk), .adc_mic_cs(adc_cs), .adc_mic_sd(adc_sd));
 
     // period is us 1000 = 0.001s = 1000hz
-    adc_model #(.PERIOD(2000)) adc_model_0(.run(run), .clk(adc_clk), .cs(adc_cs), .sd(adc_sd), .done(done));
+    adc_model #(.PERIOD(1000)) adc_model_0(.run(run), .clk(adc_clk), .cs(adc_cs), .sd(adc_sd), .done(done));
 
     // clock at 20MHz
     always #25 clk = !clk;
