124cdf3fc814594072fd8c2bab8afb10d2ac92fd
matt venn <matt@mattvenn.net>
2019-10-18 13:59:18 +0200

    formal bram

--- rtl/bram.v
+++ rtl/bram.v
@@ -41,5 +41,43 @@ module bram
             d_out <= bram[r_addr];
     end
 
-endmodule
+    `ifdef FORMAL
+    // https://zipcpu.com/zipcpu/2018/07/13/memories.html
+    
+    reg f_past_valid = 0;
+    always @(posedge r_clk)
+        f_past_valid <= 1;
 
+    // allow solver to pick any address
+    (* anyconst *) wire [ADDR_W-1:0] f_addr;
+    // make a data variable
+    reg [DATA_W-1:0] f_data;
+
+    // set data variable to whats in the bram
+    initial begin
+        assume(f_data == bram[f_addr]);
+    end
+
+    // always assert that the data at the address is correct
+    always @(*)
+        assert(bram[f_addr] == f_data);
+
+    always @(posedge r_clk) 
+        if ((f_past_valid)
+            // if a read  
+            &&($past(r_en))
+            // and at the special address
+            &&($past(r_addr == f_addr)))
+        // assert the read gives correct answer
+        assert(d_out == $past(f_data));
+
+    always @(posedge w_clk)
+            // if a write  
+            if(w_en
+            // and at the special address
+            &&(w_addr == f_addr))
+        // update the data
+        f_data <= d_in;
+    `endif
+
+endmodule
